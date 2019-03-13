0c9b5d6ee5bb44b9b6e9e22d32f83713384279f4
booldook <booldook@gmail.com>
2019-03-13 13:57:42 +0900

    sctop

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
+//$(this).css({"background-color":"#fff", "box-shadow":"0 0 10px rgba(0,0,0,0.5)"});
+
 $(window).scroll(function(){
 	var scTop = $(window, document, "html", "body").scrollTop();
 	if(scTop > 150) {
-		$(".navs").stop().animate({"top": "-60px"}, 300, function(){
-			//$(this).css({"background-color":"#fff", "box-shadow":"0 0 10px rgba(0,0,0,0.5)"});
-			$(this).stop().animate({"top":0}, 300);
-		});
+		if(!$(".navs").hasClass("navs-shadow")) {
+			$(".navs").stop().animate({"top": "-60px"}, 300, function(){
+				$(this).addClass("navs-shadow");
+				$(this).stop().animate({"top":0}, 300);
+			});
+		}
 	}
 	else {
 		console.log("위");
