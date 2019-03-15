9248eff0b39601c8b6829ba19f22e1752cc4c05a
booldook <booldook@gmail.com>
2019-03-15 14:46:25 +0900

    animation 완성

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,6 @@
 //$(this).css({"background-color":"#fff", "box-shadow":"0 0 10px rgba(0,0,0,0.5)"});
+
+$(".main_tit").css({"opacity": 1, "transform":"translate(-50%, -50%)"});
 $(window).scroll(function(){
 	var scTop = $(window, document, "html", "body").scrollTop();
 	if(scTop > 150) {
@@ -14,9 +16,29 @@ $(window).scroll(function(){
 	}
 	var pos = [];
 	$(".section").each(function(i){
-		pos[i] = $(this).offset().top;
+		pos[i] = $(this).offset().top - 300;
 	});
-	console.log(pos);
+	if(scTop >= pos[1] && scTop < pos[2]) {
+		$(".about > div").css({"transform":"translateX(0)", "opacity":1});
+	}
+	else if(scTop >= pos[2] && scTop < pos[3]) {
+		$(".skill_bar").each(function(){
+			var wid = $(this).html();
+			$(this).css({"width":wid});
+		});
+	}
+	else if(scTop >= pos[3] && scTop < pos[4]) {
+		$(".work").each(function(){
+			//var i = $(this).index();
+			//$(this).css({"transition-delay": (i * 0.2)+"s"});
+			var n = Math.random() * 1.5;
+			$(this).css({"transition-delay": n+"s"});
+			$(this).css({"opacity": 1, "transform":"scale(1)"});
+		});
+	}
+	else if(scTop >= pos[4]) {
+		$(".loc_pic, .loc_cont").css({"opacity": 1, "transform":"translateY(0)"});
+	}
 });
 
 $("#bt_top").click(function(){
@@ -29,22 +51,14 @@ $(".navs").children(".hamburger").click(function(){
 });
 
 /***** Animation *****/
-$(".main_tit").css({"opacity": 1, "transform":"translate(-50%, -50%)"});
 
-$(".about > div").css({"transform":"translateX(0)", "opacity":1});
 
-$(".skill_bar").each(function(){
-	var wid = $(this).html();
-	$(this).css({"width":wid});
-});
 
-$(".work").each(function(){
-	//var i = $(this).index();
-	//$(this).css({"transition-delay": (i * 0.2)+"s"});
-	var n = Math.random() * 1.5;
-	$(this).css({"transition-delay": n+"s"});
-	$(this).css({"opacity": 1, "transform":"scale(1)"});
-});
 
-$(".loc_pic, .loc_cont").css({"opacity": 1, "transform":"translateY(0)"});
+
+
+
+
+
+
 
